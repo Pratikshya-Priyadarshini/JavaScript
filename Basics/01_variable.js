7e749ee76b33a83a8e7e71abc6bf5ecad48590c0
Pratikshya Priyadarshini <pratikshya.p2005@gmail.com>
2026-01-11 21:24:56 +0000

    variables done

---
+++ Basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "pratikshya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhubaneswar"
+let accountState
+
+
+// accountId = 2 // not allowed.
+accountEmail = "hc.com"
+accountPassword = "4567"
+accountCity = "jatni"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
