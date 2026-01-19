f1608b9a1937524792ac678417c6defadaec5a41
Pratikshya Priyadarshini <pratikshya.p2005@gmail.com>
2026-01-19 20:30:24 +0000

    #

---
+++ Basics_02/02_array.js
@@ -0,0 +1,24 @@
+const fruits = ["apple", "banana", "coconut", "dates"]
+const flowers = ["rose", "sunflower", "hibiscus", "dahlia"]
+
+// fruits.push(flowers)
+// console.log(fruits);
+
+// fruits.concat(flowers)
+// console.log("fruits.concat(flowers) : ",fruits.concat(flowers));
+
+// console.log([...fruits,...flowers]); //spread operator
+
+// const myArr = [[1,2],[2,[1,2,3,4,5],4],[4,5],[2,5]]
+// console.log(myArr.flat(1));
+// console.log(myArr.flat(Infinity));
+
+// console.log(Array.isArray("Pratikshya"));
+// console.log(Array.from("Pratikshya"));
+// console.log(Array.from({name : "Pratikshya"})); // returns an empty array cannot convert it.
+// // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
